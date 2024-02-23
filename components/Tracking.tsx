b049e5dd4e490c84b42c15fdce4b11d154fed9fb
Bartosz Gałek <bartosz.galek@allegro.pl>
2024-02-23 09:57:00 +0100

    Update Tracking.tsx

--- components/Tracking.tsx
+++ components/Tracking.tsx
@@ -1,22 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const Tracking: React.FunctionComponent = () => {
-    const [randomNumber, setRandomNumber] = useState(undefined);
-
-    useEffect(() => {
-      setRandomNumber(Math.floor(Math.random() * 100));
-    }, []);
-
-    if (!randomNumber) return null;
-    
     return (
-        <div style={{ visibility: 'hidden', height: 0, overflow: 'hidden', position: 'relative'}}>
-            <img alt="doubleclick" width="1" height="1" style={{ position: 'absolute'}}
-                 src={`https://pubads.g.doubleclick.net/activity;dc_iu=/21612525419/DFPAudiencePixel;ord=${randomNumber};dc_seg=507368552?`}
-            />
-            <img alt="fb" height="1" width="1" style={{ position: 'absolute'}}
-                 src="https://www.facebook.com/tr?id=1650870088530325&ev=PageView&noscript=1"/>
-        </div>
+        <div style={{ visibility: 'hidden', height: 0, overflow: 'hidden', position: 'relative'}}></div>
     )
 };
 
