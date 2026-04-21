66fe7d3c1fdcc717cbb51f06930c0203ba895b82
mikolaj.kmieciak <mikolaj.kmieciak@allegro.com>
2026-04-21 11:12:36 +0200

    Add JS redirect from ml.allegro.tech to ai.allegro.tech

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,14 @@ import '../styles/global.css'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />
+const CANONICAL_HOST = "ai.allegro.tech";
+
+const App = ({ Component, pageProps }) => {
+    if (typeof window !== "undefined" && window.location.hostname !== CANONICAL_HOST) {
+        window.location.replace(`https://${CANONICAL_HOST}${window.location.pathname}`);
+        return null;
+    }
+    return <Component {...pageProps} />;
+};
 
 export default App;
